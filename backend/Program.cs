4fc896e85e67db8c58725bbbf43a3a4af5422f81
dgarc1120@gmail.com <daniel@daniel.localdomain>
2025-10-22 21:48:38 -0500

    please do not break

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,62 +1,67 @@
-using backend.Middleware;
-using backend.Models;
-using Microsoft.EntityFrameworkCore;
+    using backend.Middleware;
+    using backend.Models;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.AspNetCore.Routing; //delete this later [DANIEL]
 
-var builder = WebApplication.CreateBuilder(args);
+    var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-builder.Services.AddDbContext<ApplicationDBContext>((options) =>
-{
-    options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), ServerVersion.Parse("8.0.28-mysql"));
-});
-
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(builder =>
+    // Add services to the container.
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddControllers();
+    builder.Services.AddOpenApi();
+    builder.Services.AddDbContext<ApplicationDBContext>((options) =>
     {
-        builder.AllowAnyOrigin()
-            .AllowAnyHeader()
-            .AllowAnyMethod();
+        options.UseMySql(builder.Configuration.GetConnectionString("DefaultConnection"), ServerVersion.Parse("8.0.28-mysql"));
     });
 
-    options.AddPolicy(name: "AllowSpecificOrigins", builder =>
+    builder.Services.AddCors(options =>
     {
-        builder.WithOrigins("http://127.0.0.1:5173")
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials();
+        options.AddDefaultPolicy(builder =>
+        {
+            
+            builder.AllowAnyOrigin()
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+
+        options.AddPolicy(name: "AllowSpecificOrigins", builder =>
+        {
+            builder.WithOrigins("http://127.0.0.1:5173")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
     });
-});
 
-var app = builder.Build();
+    var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
+    // Configure the HTTP request pipeline.
 
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
-app.UseCors();
+    if (app.Environment.IsDevelopment())
+    {
+        app.MapOpenApi();
+    }
 
-app.UseHttpsRedirection();
+    app.UseCors();
 
-app.UseWhen(context => context.Request.Path.StartsWithSegments("/api/authtest"), appBuilder =>
-{
-    appBuilder.UseMiddleware<AuthenticationHandler>();
-}
-);
+    //app.UseHttpsRedirection();
 
-app.MapControllers();
+    app.UseWhen(context => context.Request.Path.StartsWithSegments("/api/authtest"), appBuilder =>
+    {
+        appBuilder.UseMiddleware<AuthenticationHandler>();
+    }
+    );
 
-app.Run();
+    app.MapControllers();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+    
+
+
+    app.Run();
+
+    record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+    {
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
