f2f21b8b3c3d3e1de983d9a479fe9a24f8de2e91
alrightytighty <ileikpotatozzzz@gmail.com>
2025-11-05 13:56:44 -0600

    added report button component for yall to use :)

--- frontend/src/Components/PlaylistCard.jsx
+++ frontend/src/Components/PlaylistCard.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styles from "./PlaylistCard.module.css";
+import ReportButton from "./ReportButton";
 
 export function PlaylistCard({ playlist }) {
   return (
     <div className={styles.card}>
+      <ReportButton right="10px" top="10px" contentId={playlist.playlistId} reportType="PLAYLIST" width="15px" height="15px"/>
       <div className={styles.coverWrapper}>
         <img src={`data:image/png;base64,${playlist.playlistImage}`} alt={playlist.playlistTitle} className={styles.cover} />
       </div>
---
+++ frontend/src/Components/ReportButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import reportSymbol from '../assets/graphics/report_button.png'
+import {useNavigate} from 'react-router'
+import styles from "./ReportButton.module.css"
+
+const ReportButton = ({
+    right,
+    left,
+    top,
+    bottom,
+    reportType,
+    width,
+    height,
+    contentId
+}) => {
+
+    const navigate = useNavigate()
+
+  return (
+    <img onClick={() => navigate(`/report?type=${reportType}&id=${contentId}`)} className={styles["report-button"]} src={reportSymbol} style={{width: width ?? '30px', height: height ?? '30px', right: right, left: left, top: top, bottom: bottom, position: 'absolute'}}/>
+  )
+}
+
+export default ReportButton
