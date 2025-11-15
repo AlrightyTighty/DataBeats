752c913d5cf21632d1769172272b3e6e660e1a4c
Bella <bellalcchan@gmail.com>
2025-11-14 20:41:03 -0600

    event location reflected on event card

--- frontend/src/Components/EventCard.jsx
+++ frontend/src/Components/EventCard.jsx
@@ -12,6 +12,7 @@ export default function EventCard({event}) {
         }
     }, [event.eventPictureFileId]);
 
+    console.log(event);
     return <button type="button" className="event" onClick={() => {navigate(`/event/${event.eventId}`)}}>
         <div className="event-promo">
             <img src={imgSrc} alt="event promo"/>
@@ -19,6 +20,8 @@ export default function EventCard({event}) {
         <div className="event-info">
             <h3>{event.title}</h3>
             <p>
+                <b>Location: {event.eventLocation}</b>
+                <br></br>
                 <b>Time: {(new Date(event.eventTime)).toLocaleString()}</b>
                 <br></br>
                 <br></br>
